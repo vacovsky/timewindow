371a7085967f1499b60d0f47ae16878ba4d8a486
jvacovsk@calpoly.edu <jvacovsk@calpoly.edu>
2018-05-02 11:30:25 -0700

    init

---
+++ timewindow.go
@@ -0,0 +1,40 @@
+package window
+
+import (
+	"time"
+)
+
+type config struct {
+	Open  time.Time
+	Close time.Time
+}
+
+func readConfig() {
+
+}
+
+func windowOpen(conf config) bool {
+
+	now := time.Now()
+
+	t1 := time.Date(now.Year(), now.Month(), now.Day(),
+		conf.Open.Hour(), conf.Open.Minute(),
+		0, 0, time.Local).Unix()
+
+	t2 := time.Date(now.Year(), now.Month(), now.Day(),
+		conf.Close.Hour(), conf.Close.Minute(),
+		0, 0, time.Local).Unix()
+
+	if t1 >= t2 {
+		// case: window spans midnight, window is open before t1 and after t2
+		if now.Unix() <= t1 || now.Unix() >= t2 {
+			return true
+		}
+	} else if t2 >= t1 {
+		// case: window open and close are same day
+		if now.Unix() >= t1 || now.Unix() <= t2 {
+			return true
+		}
+	}
+	return false
+}
---
+++ window_test.go
@@ -0,0 +1,87 @@
+package timewindow
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_timeWindow(t *testing.T) {
+	type args struct {
+		conf config
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now(),
+					Close: time.Now(),
+				},
+			},
+			want: true,
+			name: "same",
+		},
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now().Add(time.Minute * 5),
+					Close: time.Now().Add(time.Minute * 1),
+				},
+			},
+			want: true,
+			name: "Window is closed and spans midnight",
+		},
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now().Add(time.Minute * -3),
+					Close: time.Now().Add(time.Minute * -5),
+				},
+			},
+			want: true,
+			name: "Window is open and spans midnight",
+		},
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now().Add(time.Minute * -1),
+					Close: time.Now().Add(time.Minute * 2),
+				},
+			},
+			want: true,
+			name: "Window is open",
+		},
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now(),
+					Close: time.Now(),
+				},
+			},
+			want: true,
+			name: "Same Entries",
+		},
+		{
+			args: args{
+				conf: config{
+					Open:  time.Now().Add(time.Minute * 1),
+					Close: time.Now().Add(time.Minute * 3),
+				},
+			},
+			want: false,
+			name: "Window opens 1 minute from now",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := windowOpen(tt.args.conf)
+			if got != tt.want {
+				t.Errorf("windowOpen = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
