70d63673033adb4618df29e5ed9f8ee1bda8b033
jvacovsk@calpoly.edu <jvacovsk@calpoly.edu>
2018-05-03 09:15:55 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # timewindow
+
+A little module to do time comparisons for detecting if a Go time struct is between two other times of day.
+
+## the thing
+
+ BetweenTimes(time, open, close) returns a boolean value indicating whether the `time` value is
+ inside of the hours and minutes of the `open` window and the `close`.  If the open time comes 
+ after the close time, then the window spans midnight (example: window is open from 10PM until 2AM).
+ However, if the open time come before the close time (2AM to 10PM), then the
+ behaves naturally.
+ A return value of True means the submitted time is inside the window.
--- timewindow.go
+++ timewindow.go
@@ -4,13 +4,13 @@ import (
 	"time"
 )
 
-// WindowOpen returns a boolean value indicating whether the t value is
-// inside of the open window.  If the open time comes after the close time,
-// then the window spans midnight (example: window is open from 10PM until 2AM).
+// BetweenTimes(time, open, close) returns a boolean value indicating whether the `time` value is
+// inside of the hours and minutes of the `open` window and the `close`.  If the open time comes
+// after the close time, then the window spans midnight (example: window is open from 10PM until 2AM).
 // However, if the open time come before the close time (2AM to 10PM), then the
 // behaves naturally.
-// A return value of True means the submitted time is inside the open window.
-func WindowOpen(t, open, close time.Time) bool {
+// A return value of True means the submitted time is inside the window.func BetweenTimes(t, open, close time.Time) bool {
+func BetweenTimes(t, open, close time.Time) bool {
 
 	tu := t.Unix()
 
